ecf645c71055b6520d56cf7886ecd6c7c25d81de
Mario Gonzalez Macedo <omgm@users.noreply.github.com>
2014-03-17 21:07:19 -0700

    Create naught_for_mutation.rb

---
+++ tests/testdata/rspec/lib/naught_for_mutation.rb
@@ -0,0 +1,16 @@
+# Copyright (c) 2013 Avdi Grimm
+
+require "naught/version"
+require 'naught/null_class_builder'
+# require 'naught/null_class_builder/commands'
+require 'naught/null_class_builder/commands_for_mutation'
+
+module Naught
+  def self.build(&customization_block)
+    builder = NullClassBuilder.new
+    builder.customize(&customization_block)
+    builder.generate_class
+  end
+  module NullObjectTag
+  end
+end
