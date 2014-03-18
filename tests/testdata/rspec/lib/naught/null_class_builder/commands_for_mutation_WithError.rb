342566ca15d4b172a790f368be086f1fda95e59d
Mario Gonzalez Macedo <omgm@users.noreply.github.com>
2014-03-17 20:58:52 -0700

    Create commands_for_mutation_WithError.rb

---
+++ tests/testdata/rspec/lib/naught/null_class_builder/commands_for_mutation_WithError.rb
@@ -0,0 +1,13 @@
+# Copyright (c) 2013 Avdi Grimm
+
+require 'naught/null_class_builder/commands/define_explicit_conversions'
+require 'naught/null_class_builder/commands/define_implicit_conversions'
+# require 'naught/null_class_builder/commands/pebble.rb_mutated'
+require 'naught/null_class_builder/commands/pebble'
+require 'naught/null_class_builder/commands/predicates_return'
+require 'naught/null_class_builder/commands/singleton'
+require 'naught/null_class_builder/commands/traceable'
+require 'naught/null_class_builder/commands/mimic.rb_NoComments_WithError.rb_mutated'
+# require 'naught/null_class_builder/commands/mimic.rb_mutated'
+# require 'naught/null_class_builder/commands/mimic'
+require 'naught/null_class_builder/commands/impersonate'
