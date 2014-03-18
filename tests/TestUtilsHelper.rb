2d7c1a5a78fb820f2e8514154f54c9901a3f88b0
Mario Gonzalez Macedo <omgm@users.noreply.github.com>
2014-03-17 20:04:50 -0700

    Create TestUtilsHelper.rb

---
+++ tests/TestUtilsHelper.rb
@@ -0,0 +1,30 @@
+#
+# Copyright (c) 2014 "Ortask"
+# Mutator [http://ortask.com/mutator]
+#
+# This file is part of Mutator.
+#
+# Mutator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+module TestUtilsHelper
+
+    def TestUtilsHelper.override_setup_environment( testName )
+      
+        puts "\n#{__FILE__}::#{__method__}():#{__LINE__}: Setting up test environment for '#{testName}'"
+
+    end
+
+end
