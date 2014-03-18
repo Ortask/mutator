651520637eaa11342580f6f0496cd73a6c46647c
Mario Gonzalez Macedo <omgm@users.noreply.github.com>
2014-03-17 21:20:35 -0700

    Create LanguageAndTestStyleHandler_RUBY_RSPEC.rb

---
+++ src/LanguageAndTestStyleHandler_RUBY_RSPEC.rb
@@ -0,0 +1,32 @@
+#
+# Copyright (c) 2014 "Ortask"
+# Mutator [http://ortask.com/mutator]
+#
+# This file is part of Mutator.
+#
+# Mutator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require File.join(File.dirname(__FILE__), 'LanguageAndTestStyleHandler_RUBY')
+
+class LanguageAndTestStyleHandler_RUBY_RSPEC < LanguageAndTestStyleHandler_RUBY
+
+    def getTestStyle
+        return "rspec"
+    end
+
+    def post_initialize
+      @testRunner = "rspec"
+    end
+end
